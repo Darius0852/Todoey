980dde5946606a2ee71d9807a0569c39635a7fd5
Darius Ghomashchian <Darius0852@yahoo.co.uk>
2019-05-17 16:12:30 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by darius ghomashchian on 17/05/2019.
+//  Copyright © 2019 darius ghomashchian. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}

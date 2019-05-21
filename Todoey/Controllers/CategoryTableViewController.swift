16dc106ce89399e3df547d157d8120913b6c3f72
Darius Ghomashchian <Darius0852@yahoo.co.uk>
2019-05-21 14:19:26 +0100

    Persis data using Realm (including checkmark status data)

---
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by darius ghomashchian on 18/05/2019.
+//  Copyright © 2019 darius ghomashchian. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class CategoryTableViewController: UITableViewController {
+    
+    let realm = try! Realm()
+
+    
+    var categoryArray : Results<Category>?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         loadCategories()
+        
+    }
+    
+
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categoryArray?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categoryArray?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        //Turnary operator ==>
+        //value = condition? valueIfTrue: valueIfFalse
+      //  cell.accessoryType = Category.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray?[indexPath.row] 
+        }
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    func save(category: Category) {
+        
+        
+        do {
+            try realm.write {
+                realm.add(category)
+            }
+        } catch {
+            print("Error Saving Category \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories() {
+       
+        categoryArray = realm.objects(Category.self)
+        
+        tableView.reloadData()
+    }
+
+    //MARK: - Add New Categories
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item on UI alert
+            
+            
+            
+            let newCategory = Category()
+            newCategory.name = textField.text!
+            
+            
+            self.save(category: newCategory)
+            
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add a new category"
+            print(alertTextField.text)
+            textField = alertTextField
+            
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+      
+            
+        }
+        
+        
+    }
+   
+    
+
+
